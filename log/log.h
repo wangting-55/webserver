460f3829c45fac5fa752f451a46db34d96f3f7eb
wangting-55 <131342192+wangting-55@users.noreply.github.com>
2023-07-08 08:33:29 +0800

    Update log.h

--- log/log.h
+++ log/log.h
@@ -1,164 +1,69 @@
-#include <string.h>
-#include <time.h>
-#include <sys/time.h>
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
 #include <stdarg.h>
-#include "log.h"
 #include <pthread.h>
+#include "block_queue.h"
+
 using namespace std;
 
-Log::Log()
+class Log
 {
-    m_count = 0;
-    m_is_async = false;
-}
-
-Log::~Log()
-{
-    if (m_fp != NULL)
+public:
+    //C++11以后,使用局部变量懒汉不用加锁
+    static Log *get_instance()
     {
-        fclose(m_fp);
-    }
-}
-//异步需要设置阻塞队列的长度，同步不需要设置
-bool Log::init(const char *file_name, int close_log, int log_buf_size, int split_lines, int max_queue_size)
-{
-    //如果设置了max_queue_size,则设置为异步
-    if (max_queue_size >= 1)
-    {
-        m_is_async = true;
-        m_log_queue = new block_queue<string>(max_queue_size);
-        pthread_t tid;
-        //flush_log_thread为回调函数,这里表示创建线程异步写日志
-        pthread_create(&tid, NULL, flush_log_thread, NULL);
-    }
-    
-    m_close_log = close_log;
-    m_log_buf_size = log_buf_size;
-    m_buf = new char[m_log_buf_size];
-    memset(m_buf, '\0', m_log_buf_size);
-    m_split_lines = split_lines;
-
-    time_t t = time(NULL);
-    struct tm *sys_tm = localtime(&t);
-    struct tm my_tm = *sys_tm;
-
- 
-    const char *p = strrchr(file_name, '/');
-    char log_full_name[256] = {0};
-
-    if (p == NULL)
-    {
-        snprintf(log_full_name, 255, "%d_%02d_%02d_%s", my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, file_name);
-    }
-    else
-    {
-        strcpy(log_name, p + 1);
-        strncpy(dir_name, file_name, p - file_name + 1);
-        snprintf(log_full_name, 255, "%s%d_%02d_%02d_%s", dir_name, my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, log_name);
+        static Log instance;
+        return &instance;
     }
 
-    m_today = my_tm.tm_mday;
-    
-    m_fp = fopen(log_full_name, "a");
-    if (m_fp == NULL)
+    static void *flush_log_thread(void *args)
     {
-        return false;
+        Log::get_instance()->async_write_log();
     }
+    //可选择的参数有日志文件、日志缓冲区大小、最大行数以及最长日志条队列
+    bool init(const char *file_name, int close_log, int log_buf_size = 8192, int split_lines = 5000000, int max_queue_size = 0);
 
-    return true;
-}
+    void write_log(int level, const char *format, ...);
 
-void Log::write_log(int level, const char *format, ...)
-{
-    struct timeval now = {0, 0};
-    gettimeofday(&now, NULL);
-    time_t t = now.tv_sec;
-    struct tm *sys_tm = localtime(&t);
-    struct tm my_tm = *sys_tm;
-    char s[16] = {0};
-    switch (level)
+    void flush(void);
+
+private:
+    Log();
+    virtual ~Log();
+    void *async_write_log()
     {
-    case 0:
-        strcpy(s, "[debug]:");
-        break;
-    case 1:
-        strcpy(s, "[info]:");
-        break;
-    case 2:
-        strcpy(s, "[warn]:");
-        break;
-    case 3:
-        strcpy(s, "[erro]:");
-        break;
-    default:
-        strcpy(s, "[info]:");
-        break;
-    }
-    //写入一个log，对m_count++, m_split_lines最大行数
-    m_mutex.lock();
-    m_count++;
-
-    if (m_today != my_tm.tm_mday || m_count % m_split_lines == 0) //everyday log
-    {
-        
-        char new_log[256] = {0};
-        fflush(m_fp);
-        fclose(m_fp);
-        char tail[16] = {0};
-       
-        snprintf(tail, 16, "%d_%02d_%02d_", my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday);
-       
-        if (m_today != my_tm.tm_mday)
+        string single_log;
+        //从阻塞队列中取出一个日志string，写入文件
+        while (m_log_queue->pop(single_log))
         {
-            snprintf(new_log, 255, "%s%s%s", dir_name, tail, log_name);
-            m_today = my_tm.tm_mday;
-            m_count = 0;
+            m_mutex.lock();
+            fputs(single_log.c_str(), m_fp);
+            m_mutex.unlock();
         }
-        else
-        {
-            snprintf(new_log, 255, "%s%s%s.%lld", dir_name, tail, log_name, m_count / m_split_lines);
-        }
-        m_fp = fopen(new_log, "a");
-    }
- 
-    m_mutex.unlock();
-
-    va_list valst;
-    va_start(valst, format);
-
-    string log_str;
-    m_mutex.lock();
-
-    //写入的具体时间内容格式
-    int n = snprintf(m_buf, 48, "%d-%02d-%02d %02d:%02d:%02d.%06ld %s ",
-                     my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday,
-                     my_tm.tm_hour, my_tm.tm_min, my_tm.tm_sec, now.tv_usec, s);
-    
-    int m = vsnprintf(m_buf + n, m_log_buf_size - 1, format, valst);
-    m_buf[n + m] = '\n';
-    m_buf[n + m + 1] = '\0';
-    log_str = m_buf;
-
-    m_mutex.unlock();
-
-    if (m_is_async && !m_log_queue->full())
-    {
-        m_log_queue->push(log_str);
-    }
-    else
-    {
-        m_mutex.lock();
-        fputs(log_str.c_str(), m_fp);
-        m_mutex.unlock();
     }
 
-    va_end(valst);
-}
+private:
+    char dir_name[128]; //路径名
+    char log_name[128]; //log文件名
+    int m_split_lines;  //日志最大行数
+    int m_log_buf_size; //日志缓冲区大小
+    long long m_count;  //日志行数记录
+    int m_today;        //因为按天分类,记录当前时间是那一天
+    FILE *m_fp;         //打开log的文件指针
+    char *m_buf;
+    block_queue<string> *m_log_queue; //阻塞队列
+    bool m_is_async;                  //是否同步标志位
+    locker m_mutex;
+    int m_close_log; //关闭日志
+};
 
-void Log::flush(void)
-{
-    m_mutex.lock();
-    //强制刷新写入流缓冲区
-    fflush(m_fp);
-    m_mutex.unlock();
-}
+#define LOG_DEBUG(format, ...) if(0 == m_close_log) {Log::get_instance()->write_log(0, format, ##__VA_ARGS__); Log::get_instance()->flush();}
+#define LOG_INFO(format, ...) if(0 == m_close_log) {Log::get_instance()->write_log(1, format, ##__VA_ARGS__); Log::get_instance()->flush();}
+#define LOG_WARN(format, ...) if(0 == m_close_log) {Log::get_instance()->write_log(2, format, ##__VA_ARGS__); Log::get_instance()->flush();}
+#define LOG_ERROR(format, ...) if(0 == m_close_log) {Log::get_instance()->write_log(3, format, ##__VA_ARGS__); Log::get_instance()->flush();}
+
+#endif
